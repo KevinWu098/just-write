bd3d2f53f1c3c7e9ac9c240fd091a85a5024c40e
Kevin Wu <kevinwu098@gmail.com>
2025-12-24 18:04:11 -0800

    fix: env

--- src/env.ts
+++ src/env.ts
@@ -7,15 +7,22 @@ export const env = createEnv({
     client: {
         NEXT_PUBLIC_NODE_ENV: NODE_ENV,
         NEXT_PUBLIC_CONVEX_URL: z.string().min(1),
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
     },
     server: {
         NODE_ENV: NODE_ENV,
         CONVEX_DEPLOYMENT: z.string().min(1),
+        CLERK_SECRET_KEY: z.string().min(1),
+        CLERK_JWT_ISSUER_DOMAIN: z.string().min(1),
     },
     runtimeEnv: {
         NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV,
         NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+            process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
         NODE_ENV: process.env.NODE_ENV,
         CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT,
+        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+        CLERK_JWT_ISSUER_DOMAIN: process.env.CLERK_JWT_ISSUER_DOMAIN,
     },
 });
